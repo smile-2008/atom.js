36138fb49a369931bb83527bdbb0fc3513482967
vsha <v.xx.code@gmail.com>
2014-04-23 16:42:15 +0800

    enable resize

--- ui/atom-ui-plugin.js
+++ ui/atom-ui-plugin.js
@@ -32,10 +32,13 @@ var MODULE =
 
         "enableMove": function(anchorNode, moveNode, userOptions) {
 
-            var moveStatus,
+            var moveController = {}, moveStatus,
 
                 alsoMoveAnchor = false,
                 startMoved = false,
+
+                topStyle = "top",
+                leftStyle = "left",
 
                 startX, startY,
                 offsetX, offsetY,
@@ -56,9 +59,14 @@ var MODULE =
                 onMove: null,
 
                 onStartMove: null,
+                onEndMove: null,
 
-                onEndMove: null
+                useMarginTop: false,
+                useMarginLeft: false,
+
+                counter: 0
             };
+
 
             if(userOptions) {
                 copy(userOptions, Options);
@@ -70,12 +78,46 @@ var MODULE =
                 Atom.UI.Plugin.enableMove(anchorNode);
             }
 
-
             bind(onMousedown_anchorNode, anchorNode[0], "mousedown");
+
+            moveController.setOption = function(optionName, optionValue) {
+
+                Options[optionName] = optionValue;
+
+                return this;
+            }
+
+            moveController.convertMode = function(useMarginTop, left) {
+
+                if(useMarginTop == true) {
+
+                    Options.useMarginTop = true;
+
+                    topStyle = "marginTop";
+
+                }
+                else {
+
+                }
+            }
+
+
+            moveNode[0]._moveController = moveController;
+
+            return moveController;
 
             function onMousedown_anchorNode(event) {
 
                 moveStatus = "move";
+
+                Options.counter ++;
+                if(Options.useMarginTop == true) {
+                    topStyle = "marginTop";
+                }
+
+                if(Options.useMarginLeft == true) {
+                    leftStyle = "marginLeft";
+                }
 
                 // disable select
 
@@ -107,8 +149,8 @@ var MODULE =
                     case "normal":
                     {
                         DOMStyle = window.getComputedStyle(moveNode[0]);
-                        DOMStartLeft = parseInt(DOMStyle.left) || 0;
-                        DOMStartTop = parseInt(DOMStyle.top) || 0;
+                        DOMStartLeft = parseInt(DOMStyle[leftStyle]) || 0;
+                        DOMStartTop = parseInt(DOMStyle[topStyle]) || 0;
                     }
                 }
 
@@ -123,6 +165,7 @@ var MODULE =
             }
 
             function onMousemove_window(event) {
+
                 if(moveStatus == "move") {
 
 
@@ -138,8 +181,8 @@ var MODULE =
 
                     // set dom's position
 
-                    moveNode.css("left",DOMStartLeft + offsetX + "px") ;
-                    moveNode.css("top", DOMStartTop + offsetY + "px");
+                    moveNode.css(leftStyle,DOMStartLeft + offsetX + "px") ;
+                    moveNode.css(topStyle, DOMStartTop + offsetY + "px");
 
                     if(Options.onMove) {
 
@@ -178,6 +221,104 @@ var MODULE =
 
                     Options.onEndMove(event);
                 }
+            }
+        },
+
+        "enableResize": function(anchorNode, resizeNode, userOptions) {
+
+            var Options =
+            {
+                "position": "top left",
+
+                "onResize": null
+            };
+
+            $CORE.copy(userOptions, Options);
+
+            var position = Options.position.split(/\s/),
+                horizontal, vertical,
+
+                isTop, isLeft, isMousedown;
+
+            horizontal = position[1], vertical = position[0];
+
+            isTop = (vertical == "top"),
+                isLeft =(horizontal == "left");
+
+            anchorNode[0].addEventListener("mousedown", onMousedown_anchor);
+
+            function onMousedown_anchor(event) {
+
+                isMousedown = true;
+                window.addEventListener("mousemove", onMousemove_window);
+                window.addEventListener("mouseup", onMouseup_window);
+
+                // save coordinate
+
+                startX = event.screenX;
+                startY = event.screenY;
+
+                startTop = parseInt(resizeNode[0].style.top) || 0;
+                startLeft = parseInt(resizeNode[0].style.left) || 0;
+
+                startWidth = resizeNode[0].clientWidth;
+                startHeight = resizeNode[0].clientHeight;
+            }
+
+            function onMousemove_window(event) {
+
+                var newTop, newLeft, newWidth, newHeight,
+
+                    diffX, diffY;
+
+                if(isMousedown == true) {
+
+                    diffY = event.screenY - startY;
+
+                    // set new height
+
+                    if(isTop == true) {
+
+                        newTop = startTop + diffY;
+
+                        resizeNode.css("top", newTop + "px");
+
+                        diffY = -diffY;
+                    }
+
+                    newHeight = startHeight + diffY;
+                    resizeNode.css("height", newHeight + "px");
+
+                    diffX = event.screenX - startX;
+
+                    // set new width
+
+                    if(isLeft == true) {
+
+                        newLeft = startLeft + diffX;
+
+                        resizeNode.css("left", newLeft + "px");
+                        diffX = -diffX;
+                    }
+
+                    newWidth = startWidth + diffX;
+                    resizeNode.css("width", newWidth + "px");
+                }
+
+                // callback onResize
+
+                if(Options.onResize) {
+
+                    Options.onResize.call(resizeNode);
+                }
+            }
+
+            function onMouseup_window(event) {
+
+                window.removeEventListener("mouseup", onMouseup_window, false);
+                window.removeEventListener("mousemove", onMousemove_window, false);
+
+                isMousedown = false;
             }
         }
     }
